a6be33e04aa67522ec707c8cd39ce23f8ebd35f8
i.malinovskyi <i.malinovskyi@radiolux.com>
2017-08-19 22:30:42 +0300

    Write to DB new User

---
+++ resources/js/utils/registrationSubmitByName.js
@@ -0,0 +1,11 @@
+function submit_by_name() {
+    var name = document.getElementById("login").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var x = document.getElementsByName('registration');
+    x[0].submit(); //form submission
+
+    var userObejct = {name : name, email : email, password:password}
+
+    addUser(userObejct);
+}
