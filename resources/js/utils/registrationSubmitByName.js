48b067a16bfc3391c2422b8407a416029ae6f299
i.malinovskyi <i.malinovskyi@radiolux.com>
2017-08-20 12:44:21 +0300

    Creating Trip

--- resources/js/utils/registrationSubmitByName.js
+++ resources/js/utils/registrationSubmitByName.js
@@ -1,11 +1,24 @@
-function submit_by_name() {
+function createUser() {
     var name = document.getElementById("login").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var x = document.getElementsByName('registration');
     x[0].submit(); //form submission
 
-    var userObejct = {name : name, email : email, password:password};
+    var userObejct = {name: name, email: email, password: password};
 
     addUser(userObejct);
+}
+
+function createTrip() {
+    var title = document.getElementById("title").value;
+    var date = document.getElementById("date").value;
+    var cost = document.getElementById("cost").value;
+    var description = document.getElementById("description").value;
+    var x = document.getElementsByName('createRoute');
+    x[0].submit(); //form submission
+
+    var routeObejct = {title: title, date: date, cost: cost, description: description};
+
+    addRoute(routeObejct);
 }
