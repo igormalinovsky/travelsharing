faf823dea628fa68d251cf86af028ff5549bbb06
i.malinovskyi <i.malinovskyi@radiolux.com>
2017-08-20 14:04:08 +0300

    Add Trip info

--- resources/js/utils/preparingUserData.js
+++ resources/js/utils/preparingUserData.js
@@ -1,8 +1,26 @@
 function prepareSingleUserData(userEmail) {
 
-    setTimeout(function(){findUserByKey(userEmail);}, 50);
-    setTimeout(function(){
+    setTimeout(function () {
+        findUserByKey(userEmail);
+    }, 50);
+    setTimeout(function () {
         document.getElementsByClassName("user-name")[0].innerHTML = singleUserData.name;
         document.getElementsByClassName("emailInfo")[0].innerHTML = singleUserData.email
-        }, 100);
+    }, 100);
+
+    setTimeout(function () {
+        findAllRouters();
+    }, 250);
+    setTimeout(function () {
+        Object.keys(tripInfo).forEach(function (item) {
+            var parentNode = document.getElementById("user-stuff");
+            var childNode = document.createElement("section");
+            childNode.setAttribute("class", "day");
+            childNode.innerHTML = "<arcticle class=\"date\">" + tripInfo[item].date + "</arcticle>\n" +
+                "                  <arcticle class=\"event\">" + tripInfo[item].title + "</arcticle>\n" +
+                "                  <a href=\"event.html\" class=\"participate\">Приєднатись</a>\n" +
+                "                  <a href=\"#\" class=\"participate\">Цікаво</a>";
+            parentNode.appendChild(childNode)
+        })
+    }, 1000);
 }
